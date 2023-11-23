9e1cb4975cbfd377ae923d6878795894d4f890a7
SimonGrFo <simgrafor03@gmail.com>
2023-11-23 13:53:25 +0100

    1.1

---
+++ src/com/SimonGradinForsgren/oop/Main/LogicTest.java
@@ -0,0 +1,14 @@
+package com.SimonGradinForsgren.oop.Main;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LogicTest {
+
+
+
+
+}
