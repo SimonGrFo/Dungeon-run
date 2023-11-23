724d81668119c74ab0362c9191526916aa62d4d4
SimonGrFo <simgrafor03@gmail.com>
2023-11-23 16:27:22 +0100

    1.3

--- src/com/SimonGradinForsgren/oop/Main/StoryTest.java
+++ src/com/SimonGradinForsgren/oop/Main/StoryTest.java
@@ -8,6 +8,11 @@ class StoryTest {
     //Testing all story progressions (text that is displayed when moving to the next floor which
     // happens when you level up).
 
+    //I tried over the last 6 or so hours to write tests on other methods but was unable to, they run completely
+    // fine when playing the game, but I cannot find a way to make the test run them, the issue seems to be the fact
+    // almost everything uses Input and "Logic.anythingToContinue();" which requires To be Input from the user and I
+    // have not gotten it to work with Junit.
+
     @Test
     void testPrintIntro() {
         //test intro of story
